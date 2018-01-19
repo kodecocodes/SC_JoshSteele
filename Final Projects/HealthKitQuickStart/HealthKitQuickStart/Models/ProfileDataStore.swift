e8828e36b770f17a74dc4831b64798987690e0e5
Josh Steele <hococoder@gmail.com>
2018-01-18 20:19:21 -0500

    Initial commit of HealthKit Quick Start sample project

---
+++ Final Projects/HealthKitQuickStart/HealthKitQuickStart/Models/ProfileDataStore.swift
@@ -0,0 +1,132 @@
+/// Copyright (c) 2017 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import HealthKit
+
+class ProfileDataStore {
+  
+  class func getAgeSexAndBloodType() throws -> (age: Int,
+    biologicalSex: HKBiologicalSex,
+    bloodType: HKBloodType) {
+      
+      let healthKitStore = HKHealthStore()
+      
+      do {
+        
+        //1. This method throws an error if these data are not available.
+        let birthdayComponents =  try healthKitStore.dateOfBirthComponents()
+        let biologicalSex =       try healthKitStore.biologicalSex()
+        let bloodType =           try healthKitStore.bloodType()
+        
+        //2. Use Calendar to calculate age.
+        let today = Date()
+        let calendar = Calendar.current
+
+        let birthDay = calendar.date(from: birthdayComponents)
+        let ageComponents = calendar.dateComponents([.year], from: birthDay!, to: today)
+        
+        //3. Unwrap the wrappers to get the underlying enum values.
+        let unwrappedBiologicalSex = biologicalSex.biologicalSex
+        let unwrappedBloodType = bloodType.bloodType
+        
+        return (ageComponents.year!, unwrappedBiologicalSex, unwrappedBloodType)
+      }
+  }
+  
+  class func getMostRecentSample(for sampleType: HKSampleType,
+    completion: @escaping (HKQuantitySample?, Error?) -> ()) {
+    
+      //1. Use HKQuery to load the most recent samples.
+      let mostRecentPredicate = HKQuery.predicateForSamples(withStart: Date.distantPast,
+                                                            end: Date(),
+                                                            options: .strictEndDate)
+    
+      let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate,
+                                            ascending: false)
+    
+      let limit = 1
+    
+      let sampleQuery = HKSampleQuery(sampleType: sampleType,
+                                      predicate: mostRecentPredicate,
+                                      limit: limit,
+                                      sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+        DispatchQueue.main.async {
+          guard let samples = samples,
+            let mostRecentSample = samples.first as? HKQuantitySample else {
+              completion(nil, error)
+              return
+          }
+          
+          completion(mostRecentSample, nil)
+        }
+      }
+    
+      HKHealthStore().execute(sampleQuery)
+  }
+  
+  class func saveSample(value:Double, unit:HKUnit, type: HKQuantityType, date:Date)
+  {
+    let quantity = HKQuantity(unit: unit, doubleValue: value)
+    let sample = HKQuantitySample(type: type,
+                                  quantity: quantity,
+                                  start: date,
+                                  end: date)
+    
+    HKHealthStore().save(sample) { (success, error) in
+      guard let error = error else { print("Successfully saved \(type)"); return }
+      print("Error saving \(type) \(error.localizedDescription)")
+    }
+  }
+  
+  class func queryQuantitySum(for quantityType:HKQuantityType, unit:HKUnit,
+    completion: @escaping (Double?, Error?) -> Void) {
+    
+      let sumOption = HKStatisticsOptions.cumulativeSum
+      guard let startDate = Calendar.current.date(from: Calendar.current.dateComponents([.year, .month, .day], from: Date())) else {
+        fatalError("Failed to strip time from Date object")
+      }
+      let endDate = Date()
+      let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+    
+      let statisticsSumQuery = HKStatisticsQuery(quantityType: quantityType, quantitySamplePredicate: predicate, options: sumOption) {
+        (query, result, error) in
+        if let sumQuantity = result?.sumQuantity() {
+          DispatchQueue.main.async {
+            let total = sumQuantity.doubleValue(for: unit)
+            completion(total, nil)
+          }
+        }
+        else
+        {
+          print("no sum quantity")
+          completion(nil, error)
+        }
+      }
+      HKHealthStore().execute(statisticsSumQuery)
+  }
+}
---
+++ Final Projects/HealthKitQuickStart/README.md
@@ -0,0 +1,36 @@
+#  HealthKit Quick Start Screencast
+
+**This screencast was based, in part, on the HealthKit Tutorial With Swift by Ted Bendixson - if you like what you've seen here, please be sure to check out the tutorial, and follow him on twitter @bendixso**
+
+## Sample Project Idea
+A raywenderlich.com Virtual WaterCooler app that tracks how often you get up from your desk, walk around, and refill your water bottle throughout the day.  Users can enter the amount of water consumed, and the app reads how many steps you have gone and flights climbed from the HealthKit data store.  Other user profile information (height, weight, etc) is read in from HealthKit and shown in a profile page.
+
+## Main Topics
+* Privacy Settings
+* Reading static data (e.g. user profile data)
+* Querying data statistics (e.g. getting sum of all water consumed today)
+* Writing a new sample (e.g. water consumed)
+
+
+## Needs
+* Graphics for the tab bar.  One for water, one a profile image, and one my day (a calendar would work)
+
+## Notes to myself
+### Things to hit in the script
+* What can you do with HealthKit
+* Some things are emulatable on the simulator, but for a full experience, this should be deployed to a device!
+* Show diagram how multiple apps communicate with read and/or write the main data store, and how that data can get exposed to other apps with permissions
+
+
+### Project Segments
+* HealthKit Read/Write settings panel
+  * Read user stats, write water
+* Reading user information (age, etc)
+  * For display in profile page
+* Writing information to the store (water consumption)
+  * For display in tracking page, display in Health app, other water apps
+* Reading steps and motion activity
+  * For display in tracking page
+  * Needs to have observer to respond to changes
+
+
